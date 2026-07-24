9dcf4fe7e1457997fe96017fc68ec64c4b34cc87
Roshani Singh <roshanisingh1630@gmail.com>
2026-07-24 20:02:36 +0530

    Implement add task functionality

---
+++ script.js
@@ -0,0 +1,136 @@
+// ================================
+// DOM Elements
+// ================================
+
+const taskInput = document.getElementById("taskInput");
+const priority = document.getElementById("priority");
+const dueDate = document.getElementById("dueDate");
+const addTaskBtn = document.getElementById("addTaskBtn");
+
+const searchInput = document.getElementById("searchInput");
+
+const taskList = document.querySelector(".task-list");
+
+const totalTasks = document.getElementById("totalTasks");
+const completedTasks = document.getElementById("completedTasks");
+const pendingTasks = document.getElementById("pendingTasks");
+
+const filterButtons = document.querySelectorAll(".filter-btn");
+
+// ================================
+// Global Variables
+// ================================
+
+let tasks = [];
+let currentFilter = "all";
+
+// ================================
+// Event Listeners
+// ================================
+
+const taskForm = document.getElementById("taskForm");
+
+taskForm.addEventListener("submit", addTask);
+
+// ================================
+// Functions
+// ================================
+
+function addTask(event) {
+    event.preventDefault(); // Prevent form from submitting normally
+
+    const title = taskInput.value.trim();
+
+    if (title === "") {
+        alert("Please enter a task.");
+        return;
+    }
+
+    const task = {
+        id: Date.now(),
+        title: title,
+        priority: priority.value,
+        dueDate: dueDate.value,
+        completed: false
+    };
+
+    tasks.push(task);
+
+    renderTasks();
+
+    updateStats();
+
+    taskInput.value = "";
+    priority.value = "Medium";
+    dueDate.value = "";
+}
+
+function renderTasks() {
+
+    if (tasks.length === 0) {
+
+        taskList.innerHTML = `
+            <div class="empty-state">
+                <i class="fa-regular fa-clipboard"></i>
+                <h3>No Tasks Yet</h3>
+                <p>Add your first task to get started.</p>
+            </div>
+        `;
+
+        return;
+    }
+
+    taskList.innerHTML = "";
+
+    tasks.forEach(task => {
+
+        const taskCard = document.createElement("div");
+
+        taskCard.classList.add("task-card");
+
+        taskCard.innerHTML = `
+         <div class="task-info">
+             <h3>${task.title}</h3>
+
+             <div class="task-meta">
+                 <span class="priority ${task.priority.toLowerCase()}">
+                     ${task.priority}
+                 </span>
+
+                 <span>
+                    📅 ${task.dueDate || "No Due Date"}
+                 </span>
+             </div>
+         </div>
+
+         <div class="task-actions">
+             <button class="complete-btn">
+               <i class="fa-solid fa-check"></i>
+             </button>
+
+             <button class="edit-btn">
+               <i class="fa-solid fa-pen"></i>
+             </button>
+
+             <button class="delete-btn">
+               <i class="fa-solid fa-trash"></i>
+               </button>
+         </div>
+        `;
+
+        taskList.appendChild(taskCard);
+
+    });
+
+}
+
+function updateStats() {
+
+    totalTasks.textContent = tasks.length;
+
+    const completed = tasks.filter(task => task.completed).length;
+
+    completedTasks.textContent = completed;
+
+    pendingTasks.textContent = tasks.length - completed;
+}
